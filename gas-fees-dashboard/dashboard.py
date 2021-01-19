a98114f5902e9cbc2e85248c84b6c8d64175d8ab
martelev <martynenkolev@gmail.com>
2021-01-19 17:25:55 +1000

    Query exporter refactoring and documentation.

--- gas-fees-dashboard/dashboard.py
+++ gas-fees-dashboard/dashboard.py
@@ -10,7 +10,7 @@ http.client._MAXLINE = 655360 * 4
 
 load_dotenv()
 
-redash_url = os.getenv('REDASH_URL', 'http://localhost:5000')
+redashUrl = os.getenv('REDASH_URL', 'http://localhost:5000')
 setupPayload = {
     'name': os.getenv('USER_NAME', 'admin'), 'email': os.getenv('USE_EMAIL', 'admin@p2p.org'),
     'password': os.getenv('USER_PASS', 'supersecret123'), 'security_notifications': 'y',
@@ -22,7 +22,7 @@ setupHeaders = {
               'application/signed-exchange;v=b3;q=0.9'
 }
 
-setupResp = requests.post(url=redash_url+"/setup", data=setupPayload, headers=setupHeaders, allow_redirects=False)
+setupResp = requests.post(url=redashUrl + "/setup", data=setupPayload, headers=setupHeaders, allow_redirects=False)
 print('User created')
 
 ctJson = {'Content-Type': 'application/json;charset=UTF-8'}
@@ -38,69 +38,69 @@ datasourceQuery = {
 if os.getenv('DB_SSL_MODE') is not None:
     datasourceQuery['options']['sslmode'] = os.getenv('DB_SSL_MODE')
 
-# TODO: Save datasource id
-datasourceResp = requests.post(url=redash_url+"/api/data_sources", cookies=setupResp.cookies, json=datasourceQuery,
+datasourceResp = requests.post(url=redashUrl + "/api/data_sources", cookies=setupResp.cookies, json=datasourceQuery,
                                headers=ctJson)
+datasourceId = datasourceResp.json()['id']
 print('Datasource created')
 
-usersResp = requests.get(url=redash_url+"/api/users/1", cookies=setupResp.cookies)
+usersResp = requests.get(url=redashUrl + "/api/users/1", cookies=setupResp.cookies)
 apiKey = usersResp.json()['api_key']
 print('Api key:', apiKey)
 
-redash = RedashAPIClient(apiKey, redash_url)
+redash = RedashAPIClient(apiKey, redashUrl)
 
-dashboard_name = os.getenv('DASHBOARD_NAME')
-dashboard_resp = redash.create_dashboard(dashboard_name)
-dashboard_id = dashboard_resp.json()['id']
-print('Created dashboard', dashboard_name)
+dashboardName = os.getenv('DASHBOARD_NAME')
+dashboardResp = redash.create_dashboard(dashboardName)
+dashboardId = dashboardResp.json()['id']
+print('Created dashboard', dashboardName)
 
-for file_name in glob.iglob('./*.json', recursive=True):
-    f = open(file_name, "r")
-    widget_json = f.read()
-    if len(widget_json) > 0:
-        widget = json.loads(widget_json)
-        widget['dashboard_id'] = dashboard_id
-        widget_resp = redash.post('widgets', widget)
-        print('Created widget from', file_name)
+queriesDir = os.getenv('QRY_DIR', './')
+if not queriesDir.endswith('/'):
+    queriesDir += '/'
 
-for file_name in glob.iglob('./*.sql', recursive=True):
-    f = open(file_name, "r")
+for fileName in glob.iglob(queriesDir + '*.json', recursive=True):
+    f = open(fileName, "r")
+    widgetJson = f.read()
+    if len(widgetJson) > 0:
+        widget = json.loads(widgetJson)
+        widget['dashboard_id'] = dashboardId
+        widgetResp = redash.post('widgets', widget)
+        print('Created widget from', fileName)
 
-    query_name = f.readline()[2:].strip()
-    query_description = f.readline()[2:].strip()
+for fileName in glob.iglob(queriesDir + '*.sql', recursive=True):
+    f = open(fileName, "r")
+
+    queryName = f.readline()[2:].strip()
+    queryDescription = f.readline()[2:].strip()
     visualization = json.loads(f.readline()[2:].strip())
-    widget_json = f.readline()[2:].strip()
+    widgetJson = f.readline()[2:].strip()
     widget = {}
-    if len(widget_json) > 3:
-        widget = json.loads(widget_json)
+    if len(widgetJson) > 3:
+        widget = json.loads(widgetJson)
     query = f.read()
 
-    query_resp = redash.create_query(ds_id=1, name=query_name, qry=query, desc=query_description)
-    query_id = query_resp.json()['id']
-    print('Created query', query_name, 'id:', query_id)
+    queryResp = redash.create_query(ds_id=datasourceId, name=queryName, qry=query, desc=queryDescription)
+    queryId = queryResp.json()['id']
+    print('Created query', queryName, 'id:', queryId)
 
     if len(visualization) > 3:
-        visualization['query_id'] = query_id
-        vis_resp = redash.post('visualizations', visualization)
-        vis_id = vis_resp.json()['id']
-        print('Created visualisation for', query_name, 'query. Visualization id:', vis_id)
+        visualization['query_id'] = queryId
+        visResp = redash.post('visualizations', visualization)
+        visId = visResp.json()['id']
+        print('Created visualisation for', queryName, 'query. Visualization id:', visId)
 
-        redash.generate_query_results(ds_id=1, qry=query, qry_id=query_id)
-        print('Generated query results for', query_name, 'query.')
+        redash.generate_query_results(ds_id=datasourceId, qry=query, qry_id=queryId)
+        print('Generated query results for', queryName, 'query.')
 
-        publish_resp = requests.post(url="{}{}{}{}".format(redash_url, "/queries", query_id, "/source"),
-                                     cookies=setupResp.cookies, headers=ctJson,
-                                     data={'id': query_id, 'version': query_resp.json()['version'], 'is_draft': False})
+        publishResp = requests.post(url="{}{}{}{}".format(redashUrl, "/queries", queryId, "/source"),
+                                    cookies=setupResp.cookies, headers=ctJson,
+                                    data={'id': queryId, 'version': queryResp.json()['version'], 'is_draft': False})
 
-        if len(widget_json) > 3:
-            widget['dashboard_id'] = dashboard_id
-            widget['visualization_id'] = vis_id
-            widget_resp = redash.post('widgets', widget)
-            print('Created widget for', query_name, 'query')
+        if len(widgetJson) > 3:
+            widget['dashboard_id'] = dashboardId
+            widget['visualization_id'] = visId
+            widgetResp = redash.post('widgets', widget)
+            print('Created widget for', queryName, 'query')
 
-redash.publish_dashboard(dashboard_id)
-print('Published dashboard', dashboard_name)
-
-
-
-
+redash.publish_dashboard(dashboardId)
+print('Published dashboard', dashboardName)
